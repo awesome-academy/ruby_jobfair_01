8e797a8b7127bc78666b9976613996c781469da7
hoanghung123vn <20151845@student.hust.edu.vn>
2018-10-20 20:43:12 +0700

    Log in, Log out part

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,5 @@
 //= require jquery-2.2.3.min
 //= require bootstrap.min
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require_tree
--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ module RubyJobfair01
   class Application < Rails::Application
     config.load_defaults 5.2
 
-    config.i18n.default_locale = :en
     config.generators.system_tests = nil
+    config.i18n.default_locale = :en
   end
 end
